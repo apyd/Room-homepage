a2c50968e3ab1fdb7ea5e63ae1eacedbb3ee1b07
apyd <arturpydych@gmail.com>
2020-10-23 12:11:53 +0200

    Showing only intial slide on DOMContentLoaded

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,9 +2,18 @@ const header= document.querySelector('.header');
 const navBurgerButton = document.querySelector('.btn--burger');
 const prevButton = document.querySelector('.btn--left');
 const nextButton = document.querySelector('.btn--right');
+const slides = document.querySelectorAll('.slide');
+let initialSlide = 0;
+let maxSlides = slides.length - 1;
 
 window.addEventListener('scroll', () => {
     window.scrollY > 0 ? header.classList.add('header--half-transparent') : header.classList.remove('header--half-transparent');
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    slides.forEach((slide, index) => {
+        index !== initialSlide ? slides[index].classList.add('slide--inactive') : null;
+    })
 })
 
 navBurgerButton.addEventListener('click', () => {
